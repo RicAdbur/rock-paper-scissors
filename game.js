ef6e9c4d801ed7d87486eeddfe13fc97d998dde6
Max <maxrrice@hotmail.com>
2023-06-12 20:34:58 -0500

    pseudocode

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-console.log("Hello RPS");
+// Pseudocode
+
+// variables
+    // numWins
+    // numLosses
+    // numTies
+
+// 2 players; Human and Computer
+// Both players choose R, P, S
+    // Get input from Human user
+    // Random input generation from computer
+
+// Compare choices
+    // If humanChoice is S and computerChoice is P OR
+    // If humanChoice is R and computerChoice is S OR
+    // If humanChoice is P and computerChoice is R
+        // alert win
+        // increase numWins
+    //If humanChoice === computerChoice
+        // alert tie
+        // increase numTies
+    //else
+        // alert loss
+        // increase numLosses
+
+// ask user if they want to play again
+    // if yes
+        // restart game
+    //if no
+        // exit game
