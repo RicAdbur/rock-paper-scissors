38d13f6c3ed392f21276f73685ffb42664074b73
Max <maxrrice@hotmail.com>
2023-06-12 21:11:35 -0500

    added compare choices

--- game.js
+++ game.js
@@ -5,27 +5,34 @@ var numTies = 0;
 var choices = ["R", "P", "S"]
 
 // Both players choose R, P, S
+
     // Get input from Human user
 var humanChoice = prompt("Choose R, P, or S")
 console.log(humanChoice)
 
     // Random input generation from computer
 var randomNum = Math.floor( Math.random() * 3 )
-console.log( choices[randomNum] )
+var computerChoice = choices[randomNum]
+
+console.log("Human: " + humanChoice)
+console.log("Computer: " + computerChoice)
 
 
 // Compare choices
-    // If humanChoice is S and computerChoice is P OR
-    // If humanChoice is R and computerChoice is S OR
-    // If humanChoice is P and computerChoice is R
-        // alert win
-        // increase numWins
-    //If humanChoice === computerChoice
-        // alert tie
-        // increase numTies
-    //else
-        // alert loss
-        // increase numLosses
+if ( 
+    humanChoice === "S" && computerChoice === "P" ||
+    humanChoice === "R" && computerChoice === "S" ||
+    humanChoice === "P" && computerChoice === "R"
+    ) {
+    alert ("You win!")
+    // increase numWins
+} else if (humanChoice === computerChoice) {
+    alert("Round tied!")
+    // increase numTies
+} else {
+    alert("You lost!")
+    // increase numLosses
+}
 
 // ask user if they want to play again
     // if yes
